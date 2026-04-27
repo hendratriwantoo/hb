7b239ff2971f05a31bf76bcf4e952d6f701c665d
hendratriwantoo <hendratriwantoo@gmail.com>
2026-04-28 01:29:41 +0700

    Update radar_hongbao.py

--- radar_hongbao.py
+++ radar_hongbao.py
@@ -12,7 +12,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # konfigurasi default
 tele_token_default = "8682695455:AAEPyjoF9wioGM1_OhdbeawRdPCKZfUc4a8"
-chat_ids_default = "1871805510, ID_AKUN_BARU_DI_SINI"
+chat_ids_default = "1871805510"
 interval_scan = 30  # interval pengecekan dalam detik
 
 base_url_telegram = "https://api.telegram.org" 
